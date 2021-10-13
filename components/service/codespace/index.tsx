b42b81770b98661bf0111b3b8addd6e898a4ecaa
EunPyoLee1010 <dldmsvy1010@naver.com>
2021-10-13 20:41:34 +0900

    Fix terminal view

--- components/service/codespace/index.tsx
+++ components/service/codespace/index.tsx
@@ -8,7 +8,7 @@ import CodeSideBar from "./sidebar";
 import ViewSpace from "./viewspace";
 import CodeContent from "./codecontent";
 import wrapper from "../../../stores";
-import WebAssetIcon from '@material-ui/icons/WebAsset';
+import WebAssetIcon from "@material-ui/icons/WebAsset";
 import { cloneDeep, method } from "lodash";
 import { useRouter } from "next/router";
 import { useWs } from "../../context/websocket";
@@ -18,7 +18,7 @@ import { IconButton } from "@material-ui/core";
 
 export interface ISidebarItem {
     path: string;
-    children?: ISidebarItem[]
+    children?: ISidebarItem[];
 }
 
 export default function CodeSpace() {
@@ -37,6 +37,7 @@ export default function CodeSpace() {
     const router = useRouter();
     const [moveCheck, setMoveCheck] = React.useState<boolean>(false);
     const [workspaceId, setWorkspaceId] = React.useState<string>();
+    const tmpTerminalTest = React.useRef(terminalContent);
 
     const clickToChildren = (viewState: any, name: string[]) => {
         let tmpViewState: any = cloneDeep(viewState);
@@ -47,18 +48,18 @@ export default function CodeSpace() {
                     path: openId,
                     name: name[name.length - 1],
                     focus: true,
-                })
+                });
                 break;
             } else {
                 tmpViewState.wrapper[i] = clickToChildren(tmpViewState.wrapper[i], name);
             }
         }
-        return tmpViewState
-    }
+        return tmpViewState;
+    };
 
     useEffect(() => {
         // console.log(viewState)
-    }, [viewState])
+    }, [viewState]);
 
     useEffect(() => {
         if (openId === undefined) return;
@@ -84,73 +85,77 @@ export default function CodeSpace() {
                     path: openId,
                     name: name[name.length - 1],
                     focus: true,
-                })
-                setViewState(tmpViewState)
+                });
+                setViewState(tmpViewState);
             } else if (viewState.wrapper !== undefined) {
-                let forFunctionState = cloneDeep(viewState)
-                setViewState(clickToChildren(forFunctionState, name))
-
+                let forFunctionState = cloneDeep(viewState);
+                setViewState(clickToChildren(forFunctionState, name));
             } else {
                 let tmpViewState = cloneDeep(viewState);
-                tmpViewState.children = [{
-                    data: "",
-                    path: openId,
-                    name: name[name.length - 1],
-                    focus: true,
-                }]
-                setViewState(tmpViewState)
+                tmpViewState.children = [
+                    {
+                        data: "",
+                        path: openId,
+                        name: name[name.length - 1],
+                        focus: true,
+                    },
+                ];
+                setViewState(tmpViewState);
             }
         }
         setOpenId(undefined);
-    }, [openId])
+    }, [openId]);
 
     const getProjectName = async (tmpWorkspaceId: string) => {
         let tmpProjectName = await fetchSet(`/workspace?workspaceId=${tmpWorkspaceId}`, "GET", true).then((res) => res.json());
         if (tmpProjectName.code === 200) {
-            setProjectName(tmpProjectName.workspaceList[0].name)
+            setProjectName(tmpProjectName.workspaceList[0].name);
         }
-    }
+    };
 
     useEffect(() => {
         const tmpWorkSpaceId = router.query;
         if (tmpWorkSpaceId.workspaceId === undefined) {
             return;
         } else {
-            getProjectName(tmpWorkSpaceId.workspaceId as string)
-            setWorkspaceId(tmpWorkSpaceId.workspaceId as string)
+            getProjectName(tmpWorkSpaceId.workspaceId as string);
+            setWorkspaceId(tmpWorkSpaceId.workspaceId as string);
         }
-    }, [])
+    }, []);
+
+    useEffect(() => {
+        tmpTerminalTest.current = terminalContent;
+    }, [terminalContent]);
 
     const fileWebsocketHanlder = (msg: any) => {
-        const message = JSON.parse(msg.data)
-        if (message.category === 'code') {
+        const message = JSON.parse(msg.data);
+        if (message.category === "code") {
             switch (message.type) {
                 case "getAllFilePath": {
-                    setFileList(message.data)
+                    setFileList(message.data);
                     break;
                 }
             }
         } else if (message.category === "terminal") {
             switch (message.type) {
                 case "createTerminal": {
-                    let tmp = []
-                    let tmpTerminal = cloneDeep(terminalUuid)
+                    let tmp = [];
+                    let tmpTerminal = cloneDeep(terminalUuid);
                     for (let i of tmpTerminal) {
-                        tmp.push(i)
+                        tmp.push(i);
                     }
-                    tmp.push(message.data.uuid)
-                    setTerminalUuid(tmp)
+                    tmp.push(message.data.uuid);
+                    setTerminalUuid(tmp);
                     break;
                 }
                 case "commandTerminal": {
-                    let tmpContent = terminalContent;
+                    let tmpContent = cloneDeep(tmpTerminalTest.current);
                     if (tmpContent[message.data.uuid as string] === undefined) {
-                        tmpContent[message.data.uuid as string] = [message.data.message]
-
-                        setTerminalContent(tmpContent)
+                        tmpContent[message.data.uuid as string] = [message.data.message];
+                        setTerminalContent(tmpContent);
                     } else {
-                        tmpContent[message.data.uuid as string].push(message.data.message)
-                        setTerminalContent(tmpContent)
+                        tmpContent[message.data.uuid as string].push(message.data.message);
+                        setTerminalContent(tmpContent);
                     }
                     break;
                 }
@@ -158,16 +163,16 @@ export default function CodeSpace() {
                     let tmp = [];
                     for (let i of terminalUuid) {
                         if (i !== message.data.uuid) {
-                            tmp.push(i)
+                            tmp.push(i);
                         }
                     }
-                    setOpenTerminalCount(tmp.length)
-                    setTerminalUuid(tmp)
+                    setOpenTerminalCount(tmp.length);
+                    setTerminalUuid(tmp);
                     break;
                 }
             }
         }
-    }
+    };
 
     useEffect(() => {
         if (ws === undefined || openTerminalCount === 0) return;
@@ -178,112 +183,138 @@ export default function CodeSpace() {
                     type: "createTerminal",
                     data: {
                         workspaceId: workspaceId,
-                        size: { cols: 790, rows: 300 }
-                    }
+                        size: { cols: 790, rows: 300 },
+                    },
                 })
             );
         }
-    }, [ws?.readyState, openTerminalCount])
+    }, [ws?.readyState, openTerminalCount]);
 
     useEffect(() => {
         if (workspaceId === undefined) return;
         if (ws !== undefined && ws.readyState === WebSocket.OPEN) {
-            ws.addEventListener('message', fileWebsocketHanlder)
+            ws.addEventListener("message", fileWebsocketHanlder);
             ws.send(
                 JSON.stringify({
-                    category: 'code',
-                    type: 'getAllFilePath',
+                    category: "code",
+                    type: "getAllFilePath",
                     data: {
-                        workspaceId: workspaceId
-                    }
+                        workspaceId: workspaceId,
+                    },
                 })
-            )
+            );
         }
-    }, [ws?.readyState, workspaceId])
+    }, [ws?.readyState, workspaceId]);
 
-    return <div className={classes.codeWrapper}>
-        <div className={classes.topMenu}>
-            <IconButton onClick={(e) => {
-                e.stopPropagation()
-                setOpenTerminalCount(openTerminalCount + 1)
-            }} style={{
-                position: "absolute", left: "5px", top: 0, padding: 0,
-            }}>
-                <WebAssetIcon style={{ width: "20px", height: "20px", color: "#fff" }} />
-            </IconButton>
-        </div>
-        <div className={classes.contentWrapper}>
-            <CodeSideBar
-                setDragId={setDragId}
-                setOpenId={setOpenId}
-                projectName={projectName}
-                fileList={fileList}
-                focusId={focusId}
-                setFocusId={setFocusId}
-            />
-            <div style={{ width: "100%", height: "100%" }}>
-                <div style={{ width: "100%", height: openTerminalCount > 0 ? `calc(100% - ${height}px)` : "100%" }}>
-                    <div className={classes.codeConent} id="rootCode"
-                        onDragOver={(e) => {
-                            e.preventDefault();
-                            e.stopPropagation();
-                            if (dragId !== undefined) {
-                                dragComponent(e, viewState)
-                            }
-                        }}
-                        onDragLeave={(e) => {
-                            let drageventer = document.getElementById("dragEventer");
-                            if (drageventer === null) return;
-                            drageventer.style.background = "none";
-                        }}
-                        onDrop={(e) => {
-                            e.preventDefault();
-                            e.stopPropagation();
-                            e.currentTarget.style.background = "inherit"
-                            if (dragId !== undefined) {
-                                dropElement(e, viewState, setViewState, dragId)
-                            }
-                        }}>
-                        {viewState?.wrapper !== undefined && Object.keys(viewState.wrapper).map((v: string) => {
-                            if (v === undefined) return;
-                            return <ViewSpace
-                                setParentState={setViewState}
-                                setDragId={setDragId}
-                                dragId={dragId}
-                                type={v}
-                                key={v}
-                                moveCheck={moveCheck}
-                                setMoveCheck={setMoveCheck}
-                                focusId={focusId}
-                                parentState={viewState}
-                                position={{ width: (viewState.wrapper as any)[v].width, height: (viewState.wrapper as any)[v].height }}
-                                // eslint-disable-next-line react/no-children-prop
-                                children={(viewState.wrapper as any)[v]}
-                                setFocusId={setFocusId}
-                            />
-                        })}
-                        {viewState?.children !== undefined && <CodeContent
-                            setMoveCheck={setMoveCheck}
-                            dragId={dragId}
-                            setDragId={setDragId}
-                            viewState={viewState}
-                            focusId={focusId}
-                            setParentState={setViewState}
-                            setFocusId={setFocusId} />}
+    return (
+        <div className={classes.codeWrapper}>
+            <div className={classes.topMenu}>
+                <IconButton
+                    onClick={(e) => {
+                        e.stopPropagation();
+                        setOpenTerminalCount(openTerminalCount + 1);
+                    }}
+                    style={{
+                        position: "absolute",
+                        left: "5px",
+                        top: 0,
+                        padding: 0,
+                    }}
+                >
+                    <WebAssetIcon style={{ width: "20px", height: "20px", color: "#fff" }} />
+                </IconButton>
+            </div>
+            <div className={classes.contentWrapper}>
+                <CodeSideBar
+                    setDragId={setDragId}
+                    setOpenId={setOpenId}
+                    projectName={projectName}
+                    fileList={fileList}
+                    focusId={focusId}
+                    setFocusId={setFocusId}
+                />
+                <div style={{ width: "100%", height: "100%" }}>
+                    <div style={{ width: "100%", height: openTerminalCount > 0 ? `calc(100% - ${height}px)` : "100%" }}>
+                        <div
+                            className={classes.codeConent}
+                            id="rootCode"
+                            onDragOver={(e) => {
+                                e.preventDefault();
+                                e.stopPropagation();
+                                if (dragId !== undefined) {
+                                    dragComponent(e, viewState);
+                                }
+                            }}
+                            onDragLeave={(e) => {
+                                let drageventer = document.getElementById("dragEventer");
+                                if (drageventer === null) return;
+                                drageventer.style.background = "none";
+                            }}
+                            onDrop={(e) => {
+                                e.preventDefault();
+                                e.stopPropagation();
+                                e.currentTarget.style.background = "inherit";
+                                if (dragId !== undefined) {
+                                    dropElement(e, viewState, setViewState, dragId);
+                                }
+                            }}
+                        >
+                            {viewState?.wrapper !== undefined &&
+                                Object.keys(viewState.wrapper).map((v: string) => {
+                                    if (v === undefined) return;
+                                    return (
+                                        <ViewSpace
+                                            setParentState={setViewState}
+                                            setDragId={setDragId}
+                                            dragId={dragId}
+                                            type={v}
+                                            key={v}
+                                            moveCheck={moveCheck}
+                                            setMoveCheck={setMoveCheck}
+                                            focusId={focusId}
+                                            parentState={viewState}
+                                            position={{
+                                                width: (viewState.wrapper as any)[v].width,
+                                                height: (viewState.wrapper as any)[v].height,
+                                            }}
+                                            // eslint-disable-next-line react/no-children-prop
+                                            children={(viewState.wrapper as any)[v]}
+                                            setFocusId={setFocusId}
+                                        />
+                                    );
+                                })}
+                            {viewState?.children !== undefined && (
+                                <CodeContent
+                                    setMoveCheck={setMoveCheck}
+                                    dragId={dragId}
+                                    setDragId={setDragId}
+                                    viewState={viewState}
+                                    focusId={focusId}
+                                    setParentState={setViewState}
+                                    setFocusId={setFocusId}
+                                />
+                            )}
+                        </div>
+                        <div
+                            className={classes.dragEventer}
+                            id="dragEventer"
+                            onDragOver={(e) => e.preventDefault()}
+                            onDrop={(e) => e.preventDefault()}
+                        />
                     </div>
-                    <div className={classes.dragEventer} id="dragEventer"
-                        onDragOver={(e) => e.preventDefault()}
-                        onDrop={(e) => e.preventDefault()} />
+                    {openTerminalCount > 0 && (
+                        <TerminalContent
+                            setOpenContent={setOpenTerminalCount}
+                            terminalCount={openTerminalCount}
+                            content={terminalContent}
+                            height={height}
+                            uuid={terminalUuid}
+                            setHeight={setHeight}
+                            projectName={projectName}
+                        />
+                    )}
                 </div>
-                {openTerminalCount > 0 && <TerminalContent
-                    setOpenContent={setOpenTerminalCount}
-                    terminalCount={openTerminalCount}
-                    content={terminalContent}
-                    height={height}
-                    uuid={terminalUuid}
-                    setHeight={setHeight}
-                    projectName={projectName} />}
             </div>
         </div>
-    </div >
+    );
 }
--- components/service/codespace/terminal.tsx
+++ components/service/codespace/terminal.tsx
@@ -8,46 +8,54 @@ import { IconButton } from "@material-ui/core";
 import { CancelOutlined, Close, DeleteForeverOutlined } from "@material-ui/icons";
 
 function Terminal(props: any): JSX.Element {
-    return <div
-        contentEditable={false}
-        draggable={false}
-        style={{
-            height: "300px",
-            width: `${props.width}`,
-            display: "inline-block",
-            overflow: "auto",
-            outline: "none"
-        }}
-        onClick={(e) => {
-            let terminal = document.getElementById(`terminal${props.terminalCount}`);
-            if (terminal !== null) {
-                terminal.focus();
-            }
-        }}
-    >
-        {props.content[props.id]?.map((v: string) => <Ansi key={v}>{v}</Ansi>)}
-        <span
-            id={`terminal${props.terminalCount}`}
-            contentEditable={true}
-            draggable={true}
-            style={{ outline: "none", fontFamily: "monospace", fontSize: "14px" }}
-            onKeyDown={(e: any) => {
-                if (e.key === "Enter") {
-                    e.preventDefault();
-                    props.ws.send(JSON.stringify({
-                        category: "terminal",
-                        type: "commandTerminal",
-                        data: {
-                            command: e.target.innerText,
-                            id: props.id
-                        }
-                    }))
-                    e.target.innerText = ""
+    return (
+        <div
+            contentEditable={false}
+            draggable={false}
+            style={{
+                height: "300px",
+                width: `${props.width}`,
+                display: "inline-block",
+                overflow: "auto",
+                outline: "none",
+            }}
+            onClick={(e) => {
+                let terminal = document.getElementById(`terminal${props.terminalCount}`);
+                if (terminal !== null) {
+                    terminal.focus();
                 }
             }}
-        />
-
-    </div >;
+        >
+            {props.content[props.id]?.map((v: string, idx: number) => (
+                <>
+                    <Ansi key={v}>{v}</Ansi>
+                    {props.content[props.id].length - 1 !== idx && <br />}
+                </>
+            ))}
+            <span
+                id={`terminal${props.terminalCount}`}
+                contentEditable={true}
+                draggable={true}
+                style={{ outline: "none", fontFamily: "monospace", fontSize: "14px" }}
+                onKeyDown={(e: any) => {
+                    if (e.key === "Enter") {
+                        e.preventDefault();
+                        props.ws.send(
+                            JSON.stringify({
+                                category: "terminal",
+                                type: "commandTerminal",
+                                data: {
+                                    command: e.target.innerText,
+                                    id: props.id,
+                                },
+                            })
+                        );
+                        e.target.innerText = "";
+                    }
+                }}
+            />
+        </div>
+    );
 }
 
 export default function TerminalContent(props: any): JSX.Element {
@@ -63,20 +71,20 @@ export default function TerminalContent(props: any): JSX.Element {
 
     let terminalContent = [];
     for (let i = 0; i < props.terminalCount; i++) {
-        terminalContent.push(<Terminal {...props}
-            terminalCount={i}
-            ws={ws} id={props.uuid[i]} width={`${100 / props.terminalCount}%`} />)
+        terminalContent.push(<Terminal {...props} terminalCount={i} ws={ws} id={props.uuid[i]} width={`${100 / props.terminalCount}%`} />);
     }
-    return <div className={classes.terminal} style={{ height: `${props.height}px` }}>
-        <div draggable={false} className={classes.resizerBar} />
-        <IconButton
-            style={{ position: "absolute", top: "6px", right: "6px", width: "12px", height: "12px" }}
-            onClick={() => {
-                props.setOpenContent(props.terminalCount - 1)
-            }}
-        >
-            <Close style={{ width: "20px", height: "20px" }} />
-        </IconButton>
-        {terminalContent.map((v: JSX.Element) => v)}
-    </div>
+    return (
+        <div className={classes.terminal} style={{ height: `${props.height}px` }}>
+            <div draggable={false} className={classes.resizerBar} />
+            <IconButton
+                style={{ position: "absolute", top: "6px", right: "6px", width: "12px", height: "12px" }}
+                onClick={() => {
+                    props.setOpenContent(props.terminalCount - 1);
+                }}
+            >
+                <Close style={{ width: "20px", height: "20px" }} />
+            </IconButton>
+            {terminalContent.map((v: JSX.Element) => v)}
+        </div>
+    );
 }
