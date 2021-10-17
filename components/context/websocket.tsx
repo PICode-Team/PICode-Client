16c8c7b9937ed2bbbb97b78d53a13b4f0c1b5921
benovice <gkgkdnwls@kw.ac.kr>
2021-10-17 15:35:22 +0900

    hot fix websocket url bug

--- components/context/websocket.tsx
+++ components/context/websocket.tsx
@@ -10,7 +10,7 @@ export function WsProvider({ children }: any) {
   let value: any
 
   if (typeof window !== 'undefined') {
-    const ws = new WebSocket(`ws://localhost:${process.env.NEXT_PUBLIC_WS_PORT ?? 3001}`)
+    const ws = new WebSocket(`ws://${process.env.NEXT_PUBLIC_WS_URL}:${process.env.NEXT_PUBLIC_WS_PORT ?? 3001}`)
 
     ws.onopen = () => {
       if (ws!.readyState === WebSocket.OPEN) {
