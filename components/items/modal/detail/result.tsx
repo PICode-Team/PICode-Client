71de89bbd5f50c61e2691037652b82b67f1849fb
jingu kim <wlsrn3684@naver.com>
2021-10-20 22:02:18 -0700

    Add login fail modal

--- components/items/modal/detail/result.tsx
+++ components/items/modal/detail/result.tsx
@@ -2,26 +2,27 @@ import React from 'react'
 
 import Modal from '../../../items/modal/modal'
 
-interface IRequsetResultProps {
+interface IRequestResultProps {
   resultStatus: boolean
   modal: boolean
+  text: string
   setModal: React.Dispatch<React.SetStateAction<boolean>>
 }
 
-function RequsetResult(props: IRequsetResultProps) {
-  const { resultStatus, modal, setModal } = props
+function RequestResult(props: IRequestResultProps) {
+  const { resultStatus, modal, text, setModal } = props
 
   const handleSubmit = () => {
     setModal(false)
   }
 
   return (
-    <Modal modal={modal} setModal={setModal} onSubmit={handleSubmit} title={`${resultStatus === true ? 'Success' : 'Error'}`}>
+    <Modal modal={modal} setModal={setModal} onSubmit={handleSubmit} title={resultStatus === true ? 'Success' : 'Error'} hidden={true}>
       <React.Fragment>
-        <div>{resultStatus === true ? `DELETE {workspaceId}` : `ERROR in DELETE {workspaceId}`}</div>
+        <div style={{ textAlign: 'left' }}>{text}</div>
       </React.Fragment>
     </Modal>
   )
 }
 
-export default RequsetResult
+export default RequestResult
